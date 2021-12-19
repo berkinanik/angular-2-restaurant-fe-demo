777816a5fd323061e8e918b8fc6c02edb4691561
Berkin Anik <berkinanik@gmail.com>
2021-12-20 02:05:56 +0300

    added event binding and data passing between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'restaurant-fe-demo';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
